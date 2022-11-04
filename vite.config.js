e0ac6a29bbef15dc7625199f4112ff69fd016ba6
bertyn99 <bboulikou@gmail.com>
2022-11-04 11:16:51 +0100

    add manifest

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      includeAssets: ["movie.svg"],
+      manifest: {
+        name: "Movie Finder",
+        short_name: "MFinder",
+        description: "Help you to search information about a movie",
+        theme_color: "#fb923c",
+        icons: [
+          {
+            src: "movie.svg",
+            sizes: "192x192",
+            type: "image/svg",
+          },
+          {
+            src: "movie.svg",
+            sizes: "512x512",
+            type: "image/svg",
+          },
+        ],
+      },
+    }),
+  ],
+});
